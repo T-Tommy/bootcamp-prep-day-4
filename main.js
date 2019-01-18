8ef1a2f9dc2f4ec675e72bdb3ec1ca0ec00de2b3
Tommy Tran <tranttommy95@gmail.com>
2019-01-17 19:46:03 -0800

    made progress on greet function

--- main.js
+++ main.js
@@ -3,8 +3,18 @@ function greet() {
     console.log('greet function called');
 
     // get the inputs by id
+    var firstName = document.getElementById('first-name').value;
+    var middleNameInput = document.getElementById('middle-name');
+    var lastName = document.getElementById('last-name');
+
     // get value out of inputs
+    var middleName = middleNameInput.value;
+    lastName = lastName.value;
+
     // make the greeting
+    var greeting = 'Hello ' + firstName + ' ' + middleName + ' ' + lastName + '!!!';
+    console.log(greeting);
+
     // display greeting
     // remove prompt
 }
