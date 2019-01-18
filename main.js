9c7462dc6847905780ee0964a6137d8815446b67
Tommy Tran <tranttommy95@gmail.com>
2019-01-17 19:59:56 -0800

    finished greeting web page

--- main.js
+++ main.js
@@ -1,23 +1,21 @@
 // eslint-disable-next-line
 function greet() {
-    console.log('greet function called');
-
     // get the inputs by id
-    var firstName = document.getElementById('first-name').value;
-    var middleNameInput = document.getElementById('middle-name');
-    var lastName = document.getElementById('last-name');
+    var firstNameInput = document.getElementById('first-name');
+    var middleName = document.getElementById('middle-name');
+    var lastName = document.getElementById('last-name').value;
 
     // get value out of inputs
-    var middleName = middleNameInput.value;
-    lastName = lastName.value;
+    var firstName = firstNameInput.value;
+    middleName = middleName.value;
 
     // make the greeting
     var greeting = 'Hello ' + firstName + ' ' + middleName + ' ' + lastName + '!!!';
-    console.log(greeting);
 
     // display greeting
-    // remove prompt
+    var greeter = document.getElementById('greeter');
+    // changes the prompt into the greeting
+    greeter.textContent = '<h2>' + greeting + '</h2>';
 }
-
 // TO DO: validate name inputs
 // TO DO: clear inputs after greet?
